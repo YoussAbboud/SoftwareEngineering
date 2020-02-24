1ebbd5cac18a245e2421147ee004f2979479d0e1
YoussAbboud <youssef.abboud01@lau.edu>
2020-02-24 13:57:30 +0200

    front end

---
+++ js/homepage.js
@@ -0,0 +1,14 @@
+(function(){
+    "use strict";
+/*
+    Nav menu item click rollup menu on mobile
+    by: Franciscus Agnew
+    source: http://stackoverflow.com/questions/16680543/hide-twitter-bootstrap-nav-collapse-on-click
+*/
+$('.nav a').click(function() {
+    $('.navbar-collapse').collapse('hide');
+});
+
+})();
+
+
